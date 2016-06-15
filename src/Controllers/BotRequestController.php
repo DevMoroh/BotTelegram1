914f9f3cd253586aec8e8d3d26e8be68edba0c3c
modelfak <modelfak@gmail.com>
2016-06-15 11:45:26 +0300

    Добавление структуры ларавел

---
+++ src/Controllers/BotRequestController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace modelfak\BotTel\Controllers;
+use Illuminate\Routing\Controller;
+
+class BotRequestController extends Controller{
+
+    
+
+}
---
+++ src/Models/UserService.php
@@ -0,0 +1,14 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserService extends Model {
+
+
+    protected $table = 'users_service';
+
+    protected $fillable = ['first_name', 'last_name', 'external_id', 'time_create', 'service', 'status'];
+
+    
+
+}
